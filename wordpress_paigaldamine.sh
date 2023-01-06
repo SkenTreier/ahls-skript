ead71993c0b254e8416bc36fdd9edf0aba5fb8dc
SkenTreier <sken.treier@khk.ee>
2023-01-06 11:34:08 +0000

    Wordpress paigaldusskript on loodud

--- wordpress_paigaldamine.sh
+++ wordpress_paigaldamine.sh
@@ -2,10 +2,13 @@ WP=$(dpkg-query -W -f='${Status}' wordpress 2>/dev/null | grep -c 'ok installed'
 if [ $WP -eq 0 ]; then
 	echo "Paigaldame Wordpress"
 	wget https://wordpress.org/latest.tar.gz
-	tar xzvf latest.tar.gz
-	cp wordpress/wp-config-sample.php wordpress/wp-config.php
+	mkdir /var/www/html/wordpress
+	tar xzvf latest.tar.gz --strip-components=1 -C /var/www/html/wordpress
+	cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 	rm -rf latest.tar.gz
-	apt install wordpress -y
+	sed -i "s/database_name_here/wpdatabase/g" /var/www/html/wordpress/wp-config.php
+	sed -i "s/username_here/wpuser/g" /var/www/html/wordpress/wp-config.php
+	sed -i "s/password_here/qwerty/g" /var/www/html/wordpress/wp-config.php
 	echo "WordPress on paigaldatud"
 elif [ $WP -eq 1 ]; then
 	echo "Wordpress on juba paigaldatud"
